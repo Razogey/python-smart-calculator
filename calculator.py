35c2011004000072bc49fe7bad23de4798bf1cec
razogey <razogey88@gmail.com>
2026-07-19 16:00:31 +0300

    Validate arithmetic expressions and add tests

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
 class Calculator:
+    OPERATORS  = "+-*/"
 
     def tokenize(self, expression):
         tokens = []
@@ -9,7 +10,7 @@ class Calculator:
                 continue
             if char.isdigit():
                 number += char
-            elif char in ["+", "-", "*", "/"]:
+            elif char in self.OPERATORS:
                 tokens.append(int(number))
                 tokens.append(char)
                 number = ""
@@ -44,9 +45,36 @@ class Calculator:
                 i += 1
         return tokens[0]
     
-    def calculate(self, expression):
+    def validate_expression(self, expression: str):
+        expression = expression.strip()
+
+        if not expression:
+            raise ValueError("Expression cannot be empty.")
+        
         if expression == "":
-            return "Cannot Be Empty"
+            raise ValueError("Expression cannot be empty.")
+        
+        if expression[0] in self.OPERATORS:
+            raise ValueError("Expression cannot start with an operator.")
+        
+        if expression[-1] in self.OPERATORS:
+            raise ValueError("Expression cannot end with an operator.")
+        
+        for char in expression:
+            if char not in "0123456789+-*/ ":
+                raise ValueError(f"Invalid character: {char}")
+
+        for i in range(len(expression) - 1):
+            if (expression[i] in self.OPERATORS and expression[i + 1] in self.OPERATORS):
+                raise ValueError("Two consecutive operators are not allowed.")
+        
+            
+    
+    def calculate(self, expression):
+        expression = expression.replace(' ', '')
+        self.validate_expression(expression)
         tokens = self.tokenize(expression=expression)
         return self.evaluate(tokens)
 
+calc = Calculator()
+print(calc.calculate(expression="10 + 8 * 5"))
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -19,7 +19,52 @@ def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError):
         calculator.calculate("10/0")
 
+def test_empty_expression():
+    with pytest.raises(ValueError):
+        calculator.calculate("")
+    
+    with pytest.raises(ValueError):
+        calculator.calculate(" ")
+    
+def test_start_with_operator():
+    with pytest.raises(ValueError):
+        calculator.calculate("+10")    
 
+def test_expression_starts_with_multiply():
+    with pytest.raises(ValueError):
+        calculator.calculate("*10")
 
+def test_expression_ends_with_operator():
+    with pytest.raises(ValueError):
+        calculator.calculate("10+")
+
+def test_expression_ends_with_division():
+    with pytest.raises(ValueError):
+        calculator.calculate("10/")
+
+def test_invalid_character():
+    with pytest.raises(ValueError):
+        calculator.calculate("10+a")
+
+def test_invalid_symbol():
+    with pytest.raises(ValueError):
+        calculator.calculate("10&5")
+
+def test_two_consecutive_operators():
+    with pytest.raises(ValueError):
+        calculator.calculate("10++5")
+
+def test_invalid_expression():
+    with pytest.raises(ValueError):
+        calculator.calculate("5/*2")
+
+def test_simple_addition():
+    assert calculator.calculate("10+5") == 15
+
+def test_operator_precedence():
+    assert calculator.calculate("10+8*5") == 50
+
+def test_expression_with_spaces():
+    assert calculator.calculate("10 + 8 * 5") == 50
 
 
